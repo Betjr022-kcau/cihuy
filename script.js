149ccfb60fe606df2686657ef305f37cde261f23
Betjr022-kcau <abetjr038@gmail.com>
2025-01-29 23:37:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function calculate() {
+    let num1 = document.getElementById("num1").Value;
+    let num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerText = "I Love You";
+}
