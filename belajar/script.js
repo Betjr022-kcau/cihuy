9a0e2c93f296556901d65d1a12ca7eea769b91b9
Betjr022-kcau <abetjr038@gmail.com>
2025-04-21 13:15:59 +0700

    Delete belajar directory

--- belajar/script.js
+++
@@ -1,5 +0,0 @@
-function calculate() {
-    let num1 = document.getElementById("num1").Value;
-    let num2 = document.getElementById("num2").value;
-    document.getElementById("result").innerText = "I Love You";
-}
